6bb8c7cceb4ab83d0bc67f002c1ca8d8a96a22ae
Michael Osthege <michael.osthege@outlook.com>
2022-06-30 19:13:59 +0200

    Simplify `npproto.utils`

---
+++ aesara_federated/test_npproto.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+import numpy
+import pytest
+
+from aesara_federated import npproto
+from aesara_federated.npproto import utils
+
+
+class TestUtils:
+    @pytest.mark.parametrize(
+        "arr",
+        [
+            numpy.arange(5),
+            numpy.random.uniform(size=(2, 3)),
+            numpy.array(5),
+            numpy.array(["hello", "world"]),
+            numpy.array([datetime(2020, 3, 4, 5, 6, 7, 8), datetime(2020, 3, 4, 5, 6, 7, 9)]),
+            numpy.datetime64("2022-06"),
+            numpy.array([(1, 2), (3, 2, 1)], dtype=object),
+        ],
+    )
+    def test_conversion(self, arr: numpy.ndarray):
+        nda = utils.ndarray_from_numpy(arr)
+        enc = bytes(nda)
+        dec = npproto.Ndarray().parse(enc)
+        assert isinstance(dec.data, bytes)
+        result = utils.ndarray_to_numpy(dec)
+        assert result.dtype == arr.dtype
+        numpy.testing.assert_array_equal(result, arr)
+        pass
