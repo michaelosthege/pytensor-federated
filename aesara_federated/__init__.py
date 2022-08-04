e4ad4b30ae207e26901a6b549c91ec0e342b04fc
Michael Osthege <michael.osthege@outlook.com>
2022-08-04 19:49:05 +0200

    Make Aesara an optional dependency

--- aesara_federated/__init__.py
+++ aesara_federated/__init__.py
@@ -1,4 +1,7 @@
-from .op import FederatedLogpOp
+try:
+    from .op import FederatedLogpOp
+except ModuleNotFoundError:
+    pass
 from .service import FederatedLogpOpClient, FederatedLogpOpService
 
 __version__ = "0.1.0"
