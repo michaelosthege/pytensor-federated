55f7f2eb7e99dfc5ff9a91a55f052b07c25e6b6c
Michael Osthege <michael.osthege@outlook.com>
2022-10-31 18:32:21 +0100

    Run demo with worker pool and client parallelization
    Co-authored-by: Johann Fredrik Jadebeck <fredrik.jadebeck@gmail.com>

--- demo_model.py
+++ demo_model.py
@@ -1,29 +1,39 @@
 import argparse
 import logging
+from typing import Sequence
 
 import arviz
+import numpy as np
 import pymc as pm
 
-from aesara_federated import LogpGradOp, LogpGradServiceClient
+from aesara_federated import AsyncLogpGradOp, LogpGradOp, LogpGradServiceClient
 
 _log = logging.getLogger(__file__)
 logging.basicConfig(level=logging.INFO)
 
 
-def run_model(*, host: str = "127.0.0.1", port: int = 50051):
-    _log.info("Connecting to remote model at %s:%i", host, port)
-    client = LogpGradServiceClient(host, port=port)
-    _log.info("Wrapping into a LogpGradOp")
-    remote_model = LogpGradOp(client)
+def run_model(host: str, ports: Sequence[int], use_async: bool = True):
+    _log.info("Connecting to remote model on %s with %i ports.", host, len(ports))
+    client = LogpGradServiceClient(hosts_and_ports=[(host, p) for p in ports])
+    _log.info("Parallelization: %s", use_async)
+    if use_async:
+        remote_model = AsyncLogpGradOp(client.evaluate_async)
+    else:
+        remote_model = LogpGradOp(client.evaluate)
+
+    # We're building a multilevel linear regression model.
+    # The remote datasets are identical, so we're just going to offset the intercepts here.
+    N = 3
 
     with pm.Model() as pmodel:
-        intercept = pm.Normal("intercept")
+        intercept_mu = pm.Normal("intercept_mu")
+        intercept = pm.Normal("intercept", intercept_mu, sigma=0.1, size=N)
         slope = pm.Normal("slope")
-        logp, *_ = remote_model(intercept, slope)
-        pm.Potential(
-            "potential",
-            var=logp,
-        )
+
+        # Run multiple parallelizable forward passes
+        for i, off in enumerate(np.linspace(-N / 2, N / 2, N)):
+            logp, *_ = remote_model(intercept[i] + off, slope)
+            pm.Potential(f"potential_{i}", var=logp)
 
         _log.info("Running MAP estimation")
         map_ = pm.find_MAP()
@@ -41,10 +51,22 @@ if __name__ == "__main__":
         "--host", default="127.0.0.1", help="Hostname or IP address of the worker node."
     )
     parser.add_argument(
-        "--port",
-        default=50051,
-        help="Port number of the ArraysToArrays gRPC service on the worker node.",
+        "--ports",
+        default=",".join(map(str, range(50000, 50015))),
+        type=str,
+        help="Port numbers of the ArraysToArrays gRPC service workers.",
+    )
+    parser.add_argument(
+        "--parallel",
+        default="true",
+        choices=["true", "false"],
+        type=str,
+        help="Wether to use asynchronous Ops that can parallelize.",
     )
     args, _ = parser.parse_known_args()
 
-    run_model(host=args.host, port=int(args.port))
+    run_model(
+        host=args.host,
+        ports=[int(p) for p in args.ports.split(",")],
+        use_async=args.parallel.lower() == "true",
+    )
--- demo_node.py
+++ demo_node.py
@@ -1,6 +1,8 @@
 import argparse
 import asyncio
 import logging
+import multiprocessing
+import time
 from typing import Sequence, Tuple
 
 import aesara
@@ -15,10 +17,11 @@ logging.basicConfig(level=logging.INFO)
 
 
 class LinearModelBlackbox:
-    def __init__(self, data_x: np.ndarray, data_y: np.ndarray, sigma: float) -> None:
+    def __init__(self, data_x: np.ndarray, data_y: np.ndarray, sigma: float, delay: float=0) -> None:
         self._data_x = data_x
         self._data_y = data_y
         self._sigma = sigma
+        self._delay = delay
         self._fn = self._make_function(data_x, data_y, sigma)
         super().__init__()
 
@@ -41,14 +44,16 @@ class LinearModelBlackbox:
         self, *parameters: Sequence[np.ndarray]
     ) -> Tuple[np.ndarray, Sequence[np.ndarray]]:
         logp, *grads = self._fn(*parameters)
+        # Simulate expensive compute
+        time.sleep(self._delay)
         return logp, grads
 
 
-async def run_node(*, bind: str = "0.0.0.0", port: int = 50051):
+async def run_node_async(*, bind: str, port: int):
     _log.info("Generating a secret dataset")
     x = np.linspace(0, 10, 10)
     sigma = 0.4
-    y = np.random.normal(1.5 + 0.5 * x, scale=sigma)
+    y = np.random.RandomState(123).normal(1.5 + 0.5 * x, scale=sigma)
 
     import scipy.stats
 
@@ -69,15 +74,45 @@ async def run_node(*, bind: str = "0.0.0.0", port: int = 50051):
     return
 
 
+def run_node(bind_port: Tuple[str, int]):
+    try:
+        bind, port = bind_port
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(run_node_async(bind=bind, port=port))
+    except KeyboardInterrupt:
+
+        class KeyboardInterruptError(Exception):
+            pass
+
+        # Re-raise as a real exception to populate to the parent proceess.
+        raise KeyboardInterruptError()
+    return
+
+
+def run_node_pool(ports: Sequence[int]):
+    _log.info("Launching workers on %i subprocesses", len(ports))
+    pool = multiprocessing.Pool(len(ports))
+    try:
+        pool.map(run_node, [(args.bind, p) for p in ports])
+    except KeyboardInterrupt:
+        _log.info("Stopping workers...")
+        pool.terminate()
+        pool.join()
+    _log.info("All workers exited.")
+    return
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Runs a toy model as a worker node.")
     parser.add_argument(
         "--bind", default="0.0.0.0", help="IP address to run the ArraysToArrays gRPC service on."
     )
     parser.add_argument(
-        "--port", default=50051, help="Port number for the ArraysToArrays gRPC service."
+        "--ports",
+        default=",".join(map(str, range(50000, 50015))),
+        type=str,
+        help="Port numbers for the ArraysToArrays gRPC service.",
     )
     args, _ = parser.parse_known_args()
 
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(run_node(bind=args.bind, port=int(args.port)))
+    run_node_pool(list(map(int, str(args.ports).split(","))))
