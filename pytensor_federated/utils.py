db47d2bfbdb24477a39ea613214c9afda41f000f
Michael Osthege <michael.osthege@outlook.com>
2026-05-20 23:13:05 +0200

    Stop using nest-asyncio
    Running async stuff in Jupyter notebooks should work these days.

--- pytensor_federated/utils.py
+++ pytensor_federated/utils.py
@@ -3,7 +3,6 @@ import asyncio
 import logging
 from typing import Callable, Iterable, List, Optional, TypeVar
 
-import nest_asyncio
 import numpy as np
 
 T = TypeVar("T")
@@ -35,27 +34,18 @@ def argmin_none_or_func(
 
 
 def get_useful_event_loop() -> asyncio.AbstractEventLoop:
-    """Like `asyncio.get_event_loop()` but actually useful.
-
-    If the loop is already running, this function patches it using `nest_asyncio`,
-    because otherwise it would be impossible to run something on it.
-
-    Otherwise, the currently idle, or a new event loop is returned.
-
-    NOTE: This function achieves the same as running `nest_asyncio.apply()` once,
-          and using `asyncio.get_event_loop()`, but it has fewer side-effects.
-    """
-    # First try to get an already running event loop.
-    loop = asyncio._get_running_loop()
-    if loop is not None:
-        # We're already in a coroutine and `loop` is currently
-        # `await`ing the code that called this function.
-        # This loop can't `await` something new, but must be
-        # patched to support running a new loop inside.
-        if not hasattr(loop, "_nest_patched"):
-            _log.debug("Event loop is already running. Patching with nest_asyncio.")
-            nest_asyncio.apply(loop)
-    else:
-        # There is no currently active loop, so we must create a new one.
-        loop = asyncio.get_event_loop()
+    """Get a running/current/new event loop, in that order of priority."""
+    loop: asyncio.AbstractEventLoop
+    try:
+        # First try to get an already running event loop.
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        # There is no running loop.
+        try:
+            # Is there a current, not running loop?
+            loop = asyncio.get_event_loop()
+        except:
+            # There's no current loop, so we must create one.
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
     return loop
